a2d1d2d2a80c1281672ef0dc91fa44e63ed5e9f0
zkerroumi42 <za.kerroumi42@gmail.com>
2023-10-31 17:04:51 +0100

    quelques modification sur ex10 du tp2

--- TP_2/Ex10.py
+++ TP_2/Ex10.py
@@ -1,22 +1,11 @@
-L1 = [1, 3, 6, 78, 35, 88, 55, 35]
-L2 = [12, 24, 35, 24, 88, 120, 155, 35]
+L1 = [1, 3, 6, 78, 35, 88,88,88, 55, 35]
+L2 = [12, 24, 35, 24, 88,88,88, 120, 155, 35]
 L3 = []
 for i in L1:
     for j in L2:
         if j == i:
             L3.append(j)
+            if L3.count(i) > 1:
+                    L3.remove(i)
 
-L3.sort()
-for i in L1:
-    if L3.count(i) > 1:
-        L3.remove(i)
-L3.sort()
-for i in L1:
-    if L3.count(i) > 1:
-        L3.remove(i)
-
-L3.sort()
-for i in L1:
-    if L3.count(i) > 1:
-        L3.remove(i)
 print(L3)
